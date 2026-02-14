882e438450e4d10272342fcd53b7406d9d4d6ec5
Zinedis <nantenainabakari@gmail.com>
2026-02-14 23:46:45 +0300

    add file test-db for a test about database

---
+++ test-db.js
@@ -0,0 +1,12 @@
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+async function main() {
+  console.log("Tentative de connexion...")
+  const allUsers = await prisma.user.findMany()
+  console.log("Connexion réussie ! Voici les utilisateurs :", allUsers)
+}
+
+main()
+  .catch((e) => console.error("Erreur de connexion :", e))
+  .finally(async () => await prisma.$disconnect())
