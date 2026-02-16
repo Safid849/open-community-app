9b9a23c8c779f827b2d880a571794a344be489fb
Zinedis <nantenainabakari@gmail.com>
2026-02-16 20:43:34 +0300

    updated for gestion slug in url"

--- actions/community.ts
+++ actions/community.ts
@@ -1,27 +1,32 @@
 import { prisma } from "../lib/prisma";
 
 export async function getAllCommunities() {
-  try {
-    return await prisma.community.findMany({
-      orderBy: { createdAt: 'desc' }
-    });
-  } catch (error) {
-    console.error("Erreur lors de la récupération :", error);
-    return [];
-  }
+  return await prisma.community.findMany({
+    include: {
+      members: true, 
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
 }
 
 export async function createCommunity(name: string, description: string, userId: string) {
   const slug = name.toLowerCase().trim().replace(/\s+/g, '-');
 
   return await prisma.community.create({
-    data: { 
-      name: name, 
-      description: description,
-      slug: slug,
+    data: {
+      name,
+      description,
+      slug,
       admin: {
-        connect: { id: userId }
+        connect: { id: userId } 
+      },
+      members: {
+        create: {
+          userId: userId
+        }
       }
-    }
+    },
   });
 }
