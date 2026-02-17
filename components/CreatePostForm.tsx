d935bf221b935dedcfe6c1a1cde7a444b1a87db3
Zinedis <nantenainabakari@gmail.com>
2026-02-17 21:51:57 +0300

    add import react

--- components/CreatePostForm.tsx
+++ components/CreatePostForm.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import React, {useState } from "react";
 import { createPost } from "../actions/post";
 
 export default function CreatePostForm({ communityId, authorId }: { communityId: string, authorId: string }) {
