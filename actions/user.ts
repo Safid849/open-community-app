d9ba5df7c86cb44a106cb936418b80005d909e6f
Zinedis <nantenainabakari@gmail.com>
2026-02-20 07:57:52 +0300

    add user.ts

---
+++ actions/user.ts
@@ -0,0 +1,24 @@
+import { prisma } from "../lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+
+export async function syncUser() {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  const existingUser = await prisma.user.findUnique({
+    where: { id: user.id },
+  });
+
+  if (!existingUser) {
+    await prisma.user.create({
+      data: {
+        id: user.id, 
+        email: user.emailAddresses[0].emailAddress,
+        name: `${user.firstName} ${user.lastName}`,
+      },
+    });
+  }
+
+  return user.id;
+}
